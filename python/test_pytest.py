cbb9d2cb416fd0f3990a3ca4dda23437ee5a8eab
flieh <ruedelaflieh@gmail.com>
2022-01-04 17:27:14 +0100

    pytest tests

--- python/test_pytest.py
+++ python/test_pytest.py
@@ -12,4 +12,22 @@ def test_file1_method2():
 	assert x+1 == y,"test failed" 
 
 def test_helloworld():
-    assert helloworld() == 'hello world'
+    assert helloworld() == 'hello World'
+
+def test_list_fail():
+    assert [1,2,3] == [1,5,6], 'unequal lists'
+
+def test_list():
+    assert [1,2,3] == [1,2,3], 'unequal lists'
+
+def test_tuple_not_list():
+    assert (1,2,3) == [1,2,3]
+
+def test_dog_cat():
+    assert 'dog' == 'cat'
+    
+def test_5_eq_6():
+    assert '5' == '6'
+
+def test_0_eq_None():
+    assert 0 == None
